595af1ef0f52d518faabbe4d99a2ce7b9fdf4d85
alexleesonmill <alexleesonmill@gmail.com>
2020-12-06 11:52:30 +0000

    Refactored code to ES6 type.

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,23 +1,23 @@
 'use strict';
 
-describe('Bowling', function () {
+describe('Bowling', () => {
     let bowling
 
-    beforeEach(function () {
+    beforeEach(() => {
         bowling = new Bowling();
     });
 
-    it('returns a gutter game', function () {
+    it('returns a gutter game', () => {
         rollMany(0, 20)
         expect(bowling.score()).toEqual(0)
     })
 
-    it('can have a game where every roll is 2', function () {
+    it('can have a game where every roll is 2', () => {
         rollMany(2, 20)
         expect(bowling.score()).toEqual(40)
     })
 
-    it('can calculate a spare', function () {
+    it('can calculate a spare', () => {
         bowling.roll(5);
         bowling.roll(5);
         bowling.roll(2);
@@ -25,7 +25,7 @@ describe('Bowling', function () {
         expect(bowling.score()).toEqual(14)
     })
 
-    it('can calculate a strike', function () {
+    it('can calculate a strike', () => {
         bowling.roll(10);
         bowling.roll(3);
         bowling.roll(4);
@@ -33,46 +33,47 @@ describe('Bowling', function () {
         expect(bowling.score()).toEqual(24)
     })
 
-    it ('can calculate multiple strikes', function () {
+    it ('can calculate multiple strikes', () => {
         rollMany(10, 2)
         bowling.roll(3)
         rollMany(0,15)
         expect(bowling.score()).toEqual(39)
     })
 
-    it('correctly checks a perfect game', function () {
+    it('correctly checks a perfect game', () => {
         rollMany(10,12)
         expect(bowling.score()).toEqual(300)
     })
 
-    it('stops a player throwing too many rolls', function () {
+    it('stops a player throwing too many rolls', () => {
         rollMany(10, 22)
         expect(bowling.score()).toEqual(300)
     })
 
-    it('gives a bonus if strike or spare in 10th frame', function () {
+    it('gives a bonus if strike or spare in 10th frame', () => {
         rollMany(0, 18)
         rollMany(10, 3)
         expect(bowling.score()).toEqual(30)
     })
 
-    it('stops after the bonus rolls', function () {
+    it('stops after the bonus rolls', () => {
         rollMany(0, 18)
         rollMany(10, 3)
         expect(bowling.score()).toEqual(30)
     })
 
-    it('stops bonus if game doesnt end with strike', function () {
+    it('stops bonus if game doesnt end with strike', () => {
         rollMany(10, 9)
         bowling.roll(5)
         bowling.roll(4)
         expect(bowling.score()).toEqual(263)
     })
 
-    xit('allows for a partial score to be calculated', function () {
+    xit('allows for a partial score to be calculated', () => {
         rollMany(5, 3)
         expect(bowling.currentTotal()).toEqual(20);
     })
+
 
     function rollMany(pins, rolls) {
         for (let i = 0; i < rolls; i++) {
--- src/Bowling.js
+++ src/Bowling.js
@@ -6,62 +6,55 @@ class Bowling {
         this.TOTALPINS = 10;
     }
 
-    roll(pins){
-        if (this.gameOver()) {
-            throw new Error('Your game is now complete');
-        }
-        this.rolls.push(pins)
-    }
+    roll = pins => {
+        if (!this.#_gameOver()) {
+            this.rolls.push(pins)
+        } else throw new Error('Your game is now complete');
+    };
 
-    currentTotal() {
-        function add(x, y) {
-            return x + y
-        }
+    currentTotal = () => {
+        const add = (x, y) => x + y;
         return this.rolls.reduce(add, 0)
     };
 
-    score(){
-        let result = 0;
-        let rollIndex = 0;
-
-        for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
+    score = () => {
+        let result = 0, rollIndex = 0, frameIndex;
+        for (frameIndex = 0; frameIndex < 10; frameIndex++) {
             this.frame++;
-            if (this.isStrike(rollIndex)){
-                result += this.strikeScore(rollIndex);
+            if (!this.#_isStrike(rollIndex)) {
+                const frameScore = this.rolls[rollIndex] + this.rolls[rollIndex + 1]
+                if (this.#_isSpare(frameScore)) {
+                    result += this.#_spareScore(rollIndex);
+                    rollIndex += 2;
+                } else {
+                    result += frameScore;
+                    rollIndex += 2;
+                }
+            } else {
+                result += this.#_strikeScore(rollIndex);
                 rollIndex += 1;
-                continue;
-            }
-
-            const frameScore = this.rolls[rollIndex] + this.rolls[rollIndex + 1]
-            if (this.isSpare(frameScore)){
-                result += this.spareScore(rollIndex);
-                rollIndex += 2;
-            }
-            else {
-                result += frameScore;
-                rollIndex += 2;
             }
         }
         return result
-    }
+    };
 
-    gameOver(){
+    #_gameOver = () => {
         return this.rolls.count >= 21 || this.frame >= this.TOTALFRAMES;
-    }
+    };
 
-    isStrike(rollIndex){
-        return (this.rolls[rollIndex] === this.TOTALPINS);
-    }
+    #_isStrike = rollIndex => {
+        return this.rolls[rollIndex] === this.TOTALPINS;
+    };
 
-    isSpare(frameScore){
+    #_isSpare = frameScore => {
         return frameScore === this.TOTALPINS;
-    }
+    };
 
-    strikeScore(rollIndex){
-        return this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
-    }
+    #_strikeScore = rollIndex => {
+        return this.TOTALPINS + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+    };
 
-    spareScore(rollIndex){
+    #_spareScore = rollIndex => {
         return this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
-    }
+    };
 }
